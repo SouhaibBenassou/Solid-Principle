655eb033b8e7cafc3ef4c332e0db3aebceb58f12
souhaibbenassou <benassou.sohaib@gmail.com>
2024-04-16 10:55:17 +0100

    Add project files.

---
+++ DemoSingleResponsibility/After/Article.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace DemoSingleResponsibility.After
+{
+    public class Article
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+
+
+    }
+}
---
+++ DemoSingleResponsibility/Before/Article.cs
@@ -0,0 +1,19 @@
+﻿
+namespace DemoSingleResponsibility.Before
+{
+    public class Article
+    { 
+        public int Id { get; set; } 
+        public string Title { get; set; }  = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public void SaveToDB()
+        {
+
+        }
+        public void SendResponseToClient ()
+        {
+            
+        }
+    }
+}
